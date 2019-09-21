64b754029f8fdd007dc38a0c2b146720a31cb23c
maithilisavant <49150402+maithilisavant@users.noreply.github.com>
2019-09-21 13:02:48 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>User Login</title>
+</head>
+
+<body>
+<table>
+<tr>
+<th>Id</th>
+<th>First name</th>
+<th>Last name</th>
+<th>Contact Number </th>
+<th>Email</th>
+</tr>
+
+<?php
+    $conn = new mysqli('localhost', 'root', '3103', 'phpdb');
+    if(!$conn)
+    {
+        die("Connection Failed: " .mysqli_connect_error());
+    }
+
+    if(isset($_POST["submit"]))
+    {
+        $email = mysqli_real_escape_string($conn, $_POST['emailid']);  
+        $password = mysqli_real_escape_string($conn, $_POST['password']); 
+
+        $sql = "SELECT * FROM register WHERE email = '$email' and passord = '$password'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["userid"]. "</td><td>" . $row["fname"] . "</td><td>"
+            . $row["lname"]. "</td></td>" . $row["contact"]. "</td><td>" . $row["email"]. "</td></tr>" ;
+            }
+            echo "</table>";
+            } 
+            else { 
+                echo "0 results"; 
+            }
+
+    $conn->close();
+?>
---
+++ signup.php
@@ -0,0 +1,34 @@
+<?php
+    //$servername = "localhost";
+    //$username = "root";
+    //$password = "3103";
+    //$dbname = "phpdb";
+
+    $conn = new mysqli('localhost', 'root', '3103', 'phpdb');
+        if(mysqli_connect_error())
+        {
+            die("Connection Failed: " .mysqli_connect_error());
+        }
+
+        if(isset($_POST["submit"]))
+        {
+            //student details
+            $fname = mysqli_real_escape_string($conn, $_POST["fname"]);
+            $lname = mysqli_real_escape_string($conn, $_POST["lname"]);
+            $cno = mysqli_real_escape_string($conn, $_POST["cno"]);
+            $emailid = mysqli_real_escape_string($conn, $_POST["emailid"]);
+            $password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+            $sql = "INSERT INTO register(fname, lname, email, contact, passord) values('$fname', '$lname', '$emailid', '$cno', '$password')";
+            
+            if($conn->query($sql) === TRUE){
+                echo " Registered Successfully";
+
+            }
+            else
+            {
+                echo "Error: ".$sql. "<br>" .$conn->error;
+            }
+        }
+        $conn->close();
+?>
